3e8d82168ef158ecde0f5f223aace1616c73e210
Ignacio Larrain <ignaciolarrainv@gmail.com>
2023-08-05 02:19:44 -0400

    Se Centra Panelpage, intento de agregar name

--- src/views/PanelPage.jsx
+++ src/views/PanelPage.jsx
@@ -1,20 +1,26 @@
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export const PanelPage = ({ handleLogout, handleRedirectToHome }) => {
+    const navigate = useNavigate();
+    const location = useLocation(); 
+    const searchParams = new URLSearchParams(location.search);
+    const name = searchParams.get('name') || 'Usuario'; 
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            handleRedirectToHome();
+        }, 1500);
+
+        return () => clearTimeout(timer);
+    }, [handleRedirectToHome]);
 
     return (
-        <div className='col-md-3 mx-auto animate__animated animate__fadeIn'>
-            <h1 className='mt-3'>Bienvenido! </h1>
-            <Button variant='primary btn-dark mb-3 mr-4' onClick={handleRedirectToHome}>
-                Página principal
-            </Button>
-
-            <Button variant='primary btn-dark mb-3' onClick={handleLogout}>
-                Cerrar sesión
-            </Button>
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="col-md-6 text-center animate__animated animate__fadeIn" style={{ marginTop: "-35vh" }}>
+                <h1 className="mt-4 mb-3">Bienvenido {name}!</h1>
+            </div>
         </div>
-
-
-    )
+    );
 };
