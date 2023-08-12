21c4016a0fb36526160c4d847b41586ad5d0e8ef
Cristopher <cristopher.vargas@gmail.comgit clone https://github.com/learning-zone/website-templates.gitgit config --global user.email cristopher.vargas@gmail.com>
2023-08-12 13:45:04 -0400

    :sparkles: Actualizacion de Backend - Se agregan varias insersiones y consultas a la BD

--- server/consultas/consultas.js
+++ server/consultas/consultas.js
@@ -10,6 +10,23 @@ const registrarUsuario = async (usuario) => {
     await pool.query(consultas, values);
   };
 
+const ingresoPosts = async (posts) => {
+    let { usuario, titulo, descripcion, img_disco } = posts;
+    const values = [usuario, titulo, descripcion, img_disco ];
+    const consultaPosts = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3, $4)";
+    await pool.query(consultaPosts, values);
+  
+}
+
+const ingresoFavoritos = async (favoritos) => {
+    let{ idusuario, idpost } = favoritos;
+    const values = [idusuario, idpost];
+    const consultaFavoritos = "INSERT INTO favoritos(fv_us_id, fv_ps_id) VALUES ($1, $2)";
+    await pool.query(consultaFavoritos, values);
+}
+
 module.exports = {
-    registrarUsuario
+    registrarUsuario,
+    ingresoPosts,
+    ingresoFavoritos
 }
